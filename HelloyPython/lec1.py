c7682bf3c8684d122c4238ce9f77f374d0dfed7d
Ирина <bojarchukova@mail.ru>
2022-10-19 10:36:44 +0800

    Galculytor

--- HelloyPython/lec1.py
+++ HelloyPython/lec1.py
@@ -92,14 +92,15 @@ print ("hello world")
 #print(text)
 #print(len(text))
 #help(int)
+
 # Функции
-# def f(x):
-#     if x==1:
-#         return "Целое" # строка str 
-#     elif x==2.3:
-#         return 23      # число int
-#     else:
-#         return
+def f(x):
+    if x==1:
+        return "Целое" # строка str 
+    elif x==2.3:
+        return 23      # число int
+    else:
+        return
 # arg=2.3
 # print(f(arg))  
 # print(type(f(arg)))      
